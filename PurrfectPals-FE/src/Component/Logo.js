14be0532b6cd2b74e66a30ecbec80bce70036408
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-10 01:13:42 +0300

    Initiating react native app
    Adding Logo component

---
+++ PurrfectPals-FE/src/Component/Logo.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+
+function Logo() {
+  return (
+    <View style={[styles.container, {
+        flexDirection: "row"
+      }]}>
+      <Image
+        source={require("../../assets/Purr-Logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>Purrfect Pals</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        padding: 20,
+        justifyContent:'center',
+        marginTop: 69,
+
+    },
+    title: {
+        color: "black",
+        // fontWeight: 300,
+        fontSize: 26,
+        textAlign: "center",
+    },
+    logo: {
+        width: 35,
+        height: 35,
+        alignContent: 'center'
+    }
+    });
+
+export default Logo;
