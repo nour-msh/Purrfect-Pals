61e14dfee9af2975ea1ee2d67640a803525447c1
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-14 11:16:38 +0300

    Adding Post component

---
+++ PurrfectPals-FE/src/Component/Post.js
@@ -0,0 +1,63 @@
+import {View,Text,StyleSheet,Image} from 'react-native';
+
+function Post(){
+    return(
+        <View style={styles.Container}>
+            <View style={styles.imageContainer}>
+                <Image source={require("../../assets/dog1.jpeg")}
+                style={styles.dog1}/>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.name}>Pet Name</Text>
+                <Text style={styles.duration}>Duration</Text>
+                <Text style={styles.description}>Description:Lorem ipsum dolor sit amet, varius gravida lectus.</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    Container:{
+    flexDirection:'row',
+    justifyContent:'center',
+    padding:100
+    },
+    dog1:{
+        width:143,
+        height:178,
+        borderRadius:20,
+    },
+    textContainer:{
+        width:161,
+        height:132,
+        backgroundColor:'white',
+        borderLeftColor:'white',
+        borderRightColor:'#e5e5e5',
+        borderBottomColor:'#e5e5e5',
+        borderTopColor:'#e5e5e5',
+        borderWidth:1,
+        borderBottomRightRadius:20,
+        borderTopRightRadius:20,
+        marginTop:25,
+        padding:15,
+    },
+    name:{
+        fontSize:24,
+    },
+    duration:{
+        fontSize:18,
+        color:'#808080',
+        fontWeight:'300'
+    },
+    description:{
+        fontSize:12,
+        color:'#808080',
+        fontWeight:'100'
+
+    }
+
+
+
+})
+
+export default Post
