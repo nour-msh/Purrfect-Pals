6949f3c8946962982f7f87ea5eeaf012d6a87987
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-15 21:16:48 +0300

    Styling review/add review button

--- PurrfectPals-FE/src/Screens/PetProfile.js
+++ PurrfectPals-FE/src/Screens/PetProfile.js
@@ -40,7 +40,51 @@ const styles=StyleSheet.create({
         position:'relative'
 
     },
-    
+    profContainer:{
+        display:'flex',
+        flexDirection:'row',
+        marginTop:100,
+        marginLeft:40
+    },
+    profImg:{
+        width:25,
+        height:25,
+        borderRadius:20
+    },
+    review:{
+        marginLeft:40,
+        width:300
+    },
+    lowerContainer:{
+        width:360,
+        height:120,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        backgroundColor:'#FF914A',
+        opacity:0.5,
+        marginTop:35,
+        position:'relative',
+
+
+    },
+    reviewButton:{
+        width:165,
+        height:63,
+        backgroundColor:'#FF914A',
+        position:'absolute',
+        marginTop:70,
+        textAlign:'center',
+        opacity:1,
+        borderRadius:20,
+        padding:20,
+        alignSelf:'center',
+
+    },
+    reviewButtonText:{
+        fontSize:18,
+        fontWeight:'400',
+        color:'white'
+    }
 })
 
 export default PetProfile
