ce62dc3716c2f6164e240c1dd152a365cd9c444e
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-21 15:35:19 +0300

    Adding icons
    Adding ScrollView
    
    Editing styling

--- PurrfectPals-FE/src/Screens/PetProfile.js
+++ PurrfectPals-FE/src/Screens/PetProfile.js
@@ -1,83 +1,98 @@
-import { View,Text,StyleSheet,Image, TouchableOpacity } from "react-native";
+import { View,Text,StyleSheet,Image, TouchableOpacity} from "react-native";
 import ProfBox from "../Component/ProfBox";
+import { Ionicons } from '@expo/vector-icons';
+import { ScrollView } from "react-native-gesture-handler";
 
-function PetProfile(){
+
+function PetProfile({ navigation}){
     return(
         <View style={styles.Container}>
-            <View>
+            <ScrollView>
+
+            <View style={styles.imageContainer}>
+                <View>
+                <Ionicons name="chevron-back-outline" size={24} color="#FF914A" onPress={() => navigation.goBack()}/>
+                </View>
+
                 <Image source={require("../../assets/dog3.jpg")}
                 style={styles.dogProfilePic}/>
                 <ProfBox name='Pet name' petType='Pet Type'/>
             </View>
+            
             <View style={styles.profContainer}>
-                <Image source={require('../../assets/profile-pic.jpeg')}
-                style={styles.profImg}/>
-                <View style={{flex:0.06}}/>
-                <Text>Name of reviewer</Text>
-            </View>
-            <View style={styles.review}>
 
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus dolor, pretium non sapien id..</Text>
-            </View>
-            <View>
+                <View style={styles.infoContainer}>
+                    <Image source={require('../../assets/profile-pic.jpeg')}
+                    style={styles.profImg}/>
+                    <View style={{flex:0.06}}/>
+                    <Text>Name of reviewer</Text>
+                </View>
+                <View style={styles.review}>
 
-                <View style={styles.lowerContainer}></View>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus dolor, pretium non sapien id..</Text>
+                </View>
+            </View>
+            <View style={styles.profContainer}>
+
+                <View style={styles.infoContainer}>
+                    <Image source={require('../../assets/profile-pic.jpeg')}
+                    style={styles.profImg}/>
+                    <View style={{flex:0.06}}/>
+                    <Text>Name of reviewer</Text>
+                </View>
+                <View style={styles.review}>
+
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus dolor, pretium non sapien id..</Text>
+                </View>
+            </View>
+            </ScrollView>
+
+
                 <TouchableOpacity style={styles.reviewButton}>
                     <Text style={styles.reviewButtonText}>Add Review</Text>
                 </TouchableOpacity>
-            </View>
         </View>
         )}
 
 const styles=StyleSheet.create({
-    Container:{
-        alignSelf:'center',
+    imageContainer:{
+        height:360,
 
     },
     dogProfilePic:{
-        width:360,
-        height:411,
-        position:'relative'
-
+        width:'100%',
+        flex:1,
+        height:360,
     },
     profContainer:{
+        padding:20,
+       flex:1,
+    },
+    infoContainer:{
         display:'flex',
         flexDirection:'row',
-        marginTop:100,
-        marginLeft:40
+        // marginTop:100,
+        // marginLeft:40,
     },
     profImg:{
         width:25,
         height:25,
         borderRadius:20
     },
-    review:{
-        marginLeft:40,
-        width:300
-    },
-    lowerContainer:{
-        width:360,
-        height:120,
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30,
-        backgroundColor:'#FF914A',
-        opacity:0.5,
-        marginTop:35,
-        position:'relative',
 
 
-    },
     reviewButton:{
-        width:165,
+        width:'50%',
         height:63,
         backgroundColor:'#FF914A',
         position:'absolute',
-        marginTop:70,
-        textAlign:'center',
         opacity:1,
         borderRadius:20,
         padding:20,
+        justifyContent:'center',
+        alignItems:'center',
         alignSelf:'center',
+        bottom:0
 
     },
     reviewButtonText:{
