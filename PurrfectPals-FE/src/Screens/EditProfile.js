0bd6aac9031e43667a9d209de0020d5ea8def188
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-21 10:36:50 +0300

    Adding back icon

--- PurrfectPals-FE/src/Screens/EditProfile.js
+++ PurrfectPals-FE/src/Screens/EditProfile.js
@@ -1,9 +1,11 @@
 import { StyleSheet,Text,View, Image} from "react-native";
 import AccessButton from "../Component/AccessButton";
+import { Ionicons } from '@expo/vector-icons';
 
-function EditProfile(){
+function EditProfile({navigation}){
     return(
-        <View>
+        <View style={styles.container}>
+            <Ionicons style={styles.editIcon} name="chevron-back-outline" size={24} color="#FF914A" onPress={() => navigation.goBack()}/>
             <View style={styles.profContainer}>
                 <Image source={require('../../assets/profile-pic.jpeg')}
                 style={styles.profPic}/>
@@ -23,12 +25,24 @@ function EditProfile(){
 }
 
 const styles=StyleSheet.create({
+    container:{
+// alignItems:'center'
+padding:40
+    },
     profContainer:{
         display:'flex',
         alignSelf:'center',
         marginTop:60,
-        marginBottom:20
+        marginBottom:20,
+        // backgroundColor:'#FF914A',
+        // height:261,
+        // width:360
 
+    },
+    editIcon:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between'
     },
     profPic:{
         width:135,
@@ -40,8 +54,9 @@ const styles=StyleSheet.create({
         padding:10
     },
     info:{
-        padding:20,
-        color:'#808080'
+        // padding:20,
+        color:'#808080',
+        marginBottom:25
     },
     map:{
         fontSize:13,
