93d1c78702bf1a5422587eb2db98069dc6d260c9
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-26 23:41:50 +0300

    Adding onboarding to stacks/messages to bottom tabs

--- PurrfectPals-FE/src/Navigation/tabs.js
+++ PurrfectPals-FE/src/Navigation/tabs.js
@@ -1,73 +1,106 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Feed from '../Screens/Feed';
-import Profile from '../Screens/Profile';
+import { useEffect, useState, useContext } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Feed from "../Screens/Feed";
+import Profile from "../Screens/Profile";
 import VetProfile from "../Screens/VetProfile";
 import PetProfile from "../Screens/PetProfile";
-import EditProfile from '../Screens/EditProfile';
-import LogIn from '../Screens/LogIn';
-import SignUp from '../Screens/SignUp';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import EditProfile from "../Screens/EditProfile";
+import LogIn from "../Screens/LogIn";
+import SignUp from "../Screens/SignUp";
+import chatComponent from "../Screens/chatComponent";
+import OnboardingItems from "../../onBoarding/OnBoardingItems";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { UserContext } from "../../App";
 
-// import Messages from '../Screens/Messages';
-
-const Tab= createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function Stacks() {
-    return (
-      <Stack.Navigator  initialRouteName="LogIn" screenOptions={{headerShown:false}} >
-        <Stack.Screen name="LogIn" component={LogIn}/>
-        <Stack.Screen name="SignUp" component={SignUp}/>
-        <Stack.Screen name="Feed" component={Feed} />
-        <Stack.Screen name="VetProfile" component={VetProfile}/>
-        <Stack.Screen name="PetProfile" component={PetProfile} />
-        <Stack.Screen name="EditProfile" component={EditProfile} />
-        <Stack.Screen name="Tabs" component={Tabs} />
+  return (
+    <Stack.Navigator initialRouteName="" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="OnBoardingItems" component={OnboardingItems} />
+      <Stack.Screen name="Tabs" component={Tabs} />
+      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Feed" component={Feed} />
+      <Stack.Screen name="VetProfile" component={VetProfile} />
+      <Stack.Screen name="PetProfile" component={PetProfile} />
+      <Stack.Screen name="EditProfile" component={EditProfile} />
+    </Stack.Navigator>
+  );
+}
 
-
-      </Stack.Navigator>
-    );
-  }
 function Tabs() {
-    return (
-      <Tab.Navigator 
-      
-      initialRouteName="Feed"
+  const value_token = AsyncStorage.getItem("@id_token");
+
+  const { id_token, toggleState } = useContext(UserContext);
+  const [trigger, setTrigger] = useState(false);
+  useEffect(() => {
+    setTrigger(!trigger);
+  }, [id_token]);
+  return (
+    <Tab.Navigator
+      initialRouteName=""
       screenOptions={{
-        headerShown:false,
-        tabBarActiveTintColor: '#FF914A',
+        headerShown: false,
+        tabBarActiveTintColor: "#FF914A",
       }}
     >
-        <Tab.Screen name="Feed"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }} />
-        {/* <Tab.Screen name="Messages" component={Messages} />
-        <Tab.Screen name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={size} />
-          ),
-          tabBarBadge: 3,
-        }}/> */}
-        <Tab.Screen name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }} />
-      </Tab.Navigator>
-    );
-  }
+      {!value_token && (
+        <Tab.Screen
+          navigationOptions={{
+            tabBarVisible: false,
+          }}
+          name="LogIn"
+          component={LogIn}
+        />
+      )}
+      {value_token && (
+        <>
+          <Tab.Screen
+            name="Feed"
+            component={Feed}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="chatComponent"
+            component={chatComponent}
+            options={{
+              tabBarLabel: "Messages",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="message"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarLabel: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="account"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+        </>
+      )}
+    </Tab.Navigator>
+  );
+}
 
-export default Stacks
+export default Stacks;
