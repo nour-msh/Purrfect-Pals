eff2c4d3d894327b551a5d7bb22764a6e514e25b
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-27 23:26:32 +0300

    Adding firebase configuration

--- PurrfectPals-FE/firebase.js
+++ PurrfectPals-FE/firebase.js
@@ -1,21 +1,18 @@
-import firebase from '@react-native-firebase/app';
+import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCtpEDT8dvXmV30I-nyp8wlRPCVe4VrAmI",
     authDomain: "purrfect-pals.firebaseapp.com",
+    databaseURL: "https://purrfect-pals-default-rtdb.firebaseio.com",
     projectId: "purrfect-pals",
     storageBucket: "purrfect-pals.appspot.com",
     messagingSenderId: "571025265245",
-    appId: "1:571025265245:web:e711871fb43bf95601f3b4"
-  };
+    appId: "1:571025265245:web:b875dee61abc39af01f3b4"
+};
 
-  let app;
-  if (firebase.apps.length===0){
-    app= firebase.initializeApp(firebaseConfig);
-  }else{
-    app= firebase.app()
-  }
-
-  const db= app.firestore();
-
-  export default db;
+initializeApp(firebaseConfig);
+export const database = getFirestore();
