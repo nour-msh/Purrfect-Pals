8f95a55b1a44a81341b95da0e1c867d455218a7d
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-27 23:31:21 +0300

    Implementing getUser function

--- PurrfectPals-BE/src/Controller/UserController.js
+++ PurrfectPals-BE/src/Controller/UserController.js
@@ -1,13 +1,24 @@
-import {User} from '../Model/User.js';
+import { User } from "../Model/User.js";
 
-export async function updateUserProfile(req,res) {
-    try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body)
-      const new_user = await User.findById(req.params.id)
-      res.status(200).send(new_user)
-    } catch (err) {
-      res.status(500).send({error: "Problem with Updating User recored "})
-    }
+export async function updateUserProfile(req, res) {
+  try {
+    console.log(req.params.id)
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    const new_user = await User.findById(req.params.id);
+    res.status(200).send(new_user);
+  } catch (err) {
+    res.status(500).send({ error: "Problem with Updating User recored " });
   }
+}
 
-  
+
+export async function getUser(req, res) {
+  try {
+    const results = await User.find();
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
