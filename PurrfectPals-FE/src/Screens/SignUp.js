e1cdbb215e53cb46b5e717ccfa68006209b9f893
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-27 14:08:55 +0300

    Adding checkbox
    Adding navigation

--- PurrfectPals-FE/src/Screens/SignUp.js
+++ PurrfectPals-FE/src/Screens/SignUp.js
@@ -1,112 +1,133 @@
-import {Text,View,Image,StyleSheet,TextInput, ToastAndroid} from 'react-native';
-import React, {useState } from 'react';
-import axios from 'axios';
+import { Text, View, Image, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import axios from "axios";
+import Checkbox from "react-native-checkbox-animated";
+import AccessButton from "../Component/AccessButton";
+import Logo from "../Component/Logo";
 
-import AccessButton from '../Component/AccessButton';
-import Logo from '../Component/Logo';
+function SignUp({ navigation }) {
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [user_type, setUser_type] = useState("");
 
+  const handleSubmitSignup = () => {
+    const data = {
+      full_name: fullName,
+      email: email,
+      password: password,
+      phone_number: phoneNumber,
+      user_type: checked ? "1" : "0",
+    };
+    //e.preventDefault();
+    axios({
+      method: "POST",
+      data,
+      url: "http://192.168.1.2:5000/user/register",
+    })
+      .then((res) => {
+        console.log(res);
+        navigation.navigate("LogIn");
+      })
+      .catch((error) => console.log(error));
+  };
 
-function SignUp({navigation}){
-    const[fullName,setFullname]=useState("");
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
-    const[phoneNumber,setPhoneNumber]=useState("");
+  const handleFullNameChange = (value) => {
+    setFullname(value)
+  }
 
-    const handleSubmitSignup = () => {
-        const data = {
-          full_name: fullName,
-          email: email,
-          password: password,
-          phone_number: phoneNumber
-        };
-        //e.preventDefault();
-        axios({
-          method: "POST",
-          data,
-          url: "http://192.168.1.4:5000/user/register",
-        })
-          .then((res) => {ToastAndroid.show(res, ToastAndroid.LONG)})
-          .catch((error) => console.log(error));
-      };
+  const handleEmailChange = (value) => {
+    setEmail(value)
+  }
 
-      const handleFullNameChange = (value) => {
-        setFullname(value)
-      }
+  const handlePasswordChange = (value) => {
+    setPassword(value)
+  }
 
-      const handleEmailChange = (value) => {
-        setEmail(value)
-      }
+  const handlePhoneNumberChange = (value) => {
+    setPhoneNumber(value)
+  }
+  const [checked, setChecked] = useState(false);
 
-      const handlePasswordChange = (value) => {
-        setPassword(value)
-      }
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Image
+        source={require("../../assets/signup-doggy.png")}
+        style={styles.dog}
+      />
 
-      const handlePhoneNumberChange = (value) => {
-        setPhoneNumber(value)
-      }
-    
-        
-    return(
-        <View style={styles.container}>
-            <Logo/>
-            <Image source={require('../../assets/signup-doggy.png')}
-            style={styles.dog}/>
+      <TextInput
+        placeholder="Full name"
+        style={styles.input}
+        value={fullName}
+        onChangeText={handleFullNameChange}
+      ></TextInput>
 
-            <TextInput placeholder='Full name' style={styles.input} 
-            value={fullName} onChangeText={handleFullNameChange}
-            ></TextInput>
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={handleEmailChange}
+      ></TextInput>
 
-            <TextInput placeholder='Email' style={styles.input}
-            value={email} onChangeText={handleEmailChange}
-             ></TextInput>
+      <TextInput
+      secureTextEntry={true}
+        placeholder="Password"
+        style={styles.input}
+        value={password}
+        onChangeText={handlePasswordChange}
+      ></TextInput>
 
-            <TextInput placeholder='Password' style={styles.input} 
-            value={password} onChangeText={handlePasswordChange}
-            ></TextInput>
+      <TextInput
+        placeholder="Phone number"
+        style={styles.input}
+        value={phoneNumber}
+        onChangeText={handlePhoneNumberChange}
+      ></TextInput>
+      <Checkbox
+        label="Are you a Vet ?"
+        onValueChange={(val) => setChecked(val)}
+        checked={checked}
+      />
+      <AccessButton buttonTitle="Sign Up" handlePress={handleSubmitSignup} />
 
-            <TextInput placeholder='Phone number' style={styles.input} 
-            value={phoneNumber} onChangeText={handlePhoneNumberChange}
-            ></TextInput>
-
-            <AccessButton buttonTitle='Sign Up' 
-             handlePress={handleSubmitSignup}
-/>
-
-            <Text style={styles.member} onPress={()=>navigation.navigate('LogIn')}>Already have an account?</Text>
-        </View>
-    )
+      <Text style={styles.member} onPress={() => navigation.navigate("LogIn")}>
+        Already have an account?
+      </Text>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-  container:{
-    padding:25
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
   },
-    dog:{
-        width:290,
-        height:176,
-        // marginLeft:33,
-        marginTop:25,
-        alignSelf:'center'
-    },
-    input:{
-        fontSize:18,
-        height:55,
-        // width:294,
-        borderRadius:8,
-        borderWidth: 1,
-        borderColor:'#e5e5e5',
-        marginTop:0,
-        // marginLeft:33,
-        padding:15,
-        marginBottom:10,
-    },
-    member:{
-        textAlign:'center',
-        color:'#CAC8C8',
-        padding:10
-    }
+  dog: {
+    width: 290,
+    height: 176,
+    // marginLeft:33,
+    marginTop: 5,
+    alignSelf: "center",
+  },
+  input: {
+    fontSize: 18,
+    height: 55,
+    // width:294,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#e5e5e5",
+    marginTop: 0,
+    // marginLeft:33,
+    padding: 15,
+    marginBottom: 5,
+  },
+  member: {
+    textAlign: "center",
+    color: "#CAC8C8",
+    padding: 10,
+  },
+});
 
-
-})
-
-export default SignUp
+export default SignUp;
