fc8722815c687be8f2de779049ff3b81a300d2bf
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-28 16:51:11 +0300

    Addding pet_id/user_id

--- PurrfectPals-BE/src/Controller/ReviewController.js
+++ PurrfectPals-BE/src/Controller/ReviewController.js
@@ -1,17 +1,20 @@
-//import { Review } from '../Model/Review.js';
+import { Review } from '../Model/Review.js';
 
 
 export async function addReview(req,res){
     try{
+        const pet_id = req.params.pet_id
+        const user_id = req.params.user_id
         const{
-            text,
-            rating
+            content,
         }=req.body;
         const review=new Review({
-            text,
-            rating
+            content,
+            pet_id,
+            user_id
         })
         await review.save();
+        console.log("added review", review)
         res.status(201).send(review);
     } catch(error){
         res.status(400).json({message:error.message});
