4f796579a0e36b21aed00e1bb8a1de32ead7327e
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-30 10:56:22 +0300

    Adding addAppointment function

--- PurrfectPals-BE/src/Controller/VetController.js
+++ PurrfectPals-BE/src/Controller/VetController.js
@@ -1,3 +1,4 @@
+import { Appointment } from '../Model/Appointment.js';
 import {User} from '../Model/User.js';
 
 export async function getVets(req,res){
@@ -8,5 +9,22 @@ export async function getVets(req,res){
     res.status(500).json({
       message: error.message
     })
+  }
+}
+
+export async function addAppointment(req,res){
+  try{
+      const{
+          date,
+          time,
+      }=req.body;
+      const appointment=new Appointment({
+          date,
+          time,
+      })
+      await appointment.save();
+      res.status(201).send(appointment);
+  } catch(error){
+      res.status(400).json({message:error.message});
   }
 }
