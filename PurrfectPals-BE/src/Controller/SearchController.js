1e9894bdcbfce9187d251a18673cd5537021ddaf
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-30 13:16:27 +0300

    Adding searchController

--- PurrfectPals-BE/src/Controller/SearchController.js
+++ PurrfectPals-BE/src/Controller/SearchController.js
@@ -1,10 +1,15 @@
-// export async function getPosts(req,res){
-//     try{
-//         const results = await Post.find({pet_name})
-//         res.status(200).json(results)
-//     } catch (error) {
-//         res.status(500).json({
-//             message: error.message
-//         })
-//     }
-// }
+import {Post} from "../Model/Post.js"
+
+export async function search(req,res){
+    try{
+        const results = await Post.find({"or":[
+            {pet_name:{$regex:req.params.key }},
+            {duration:{$regex:req.params.key }}
+        ]})
+        res.status(200).json(results)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
