a62f28d88eeca8a047d905091b4639b4a13aa59f
Nour Mshawrab <mshawrabnour@gmail.com>
2022-07-30 10:55:48 +0300

    Adding addAppointment route

--- PurrfectPals-BE/src/Routes/UserRoutes.js
+++ PurrfectPals-BE/src/Routes/UserRoutes.js
@@ -3,12 +3,11 @@ import {register, login} from "../Controller/AuthController.js";
 import {addPet, getPets} from '../Controller/PetController.js';
 import { addReview } from '../Controller/ReviewController.js';
 import { getAllReviewsByPetId } from '../Controller/ReviewController.js'
-import { addAppointment } from '../Controller/AppointmentController.js';
 import { getVets } from '../Controller/VetController.js';
 import {updateUserProfile} from '../Controller/UserController.js';
 import { addPost } from '../Controller/PostController.js';
 import { getPosts } from '../Controller/PostController.js';
-// import { getPost } from '../Controller/SearchController.js';
+import { addAppointment } from '../Controller/VetController.js';
 
 
 const router = Router();
@@ -20,12 +19,11 @@ router.post("/addPet/:user_id", addPet);
 router.get("/getPets/:user_id", getPets);
 router.post("/addReview/:user_id/:pet_id", addReview);
 router.get("/getAllReviewsByPetId/:pet_id", getAllReviewsByPetId)
-router.post("/addAppointment",addAppointment);
 router.get("/getVets", getVets);
 router.put("/updateUserProfile/:id", updateUserProfile);
 router.post("/addPost",addPost);
 router.get("/getPosts", getPosts);
-// router.get("/getPost", getPost);
+router.post("/addAppointment", addAppointment);
 
 
 export default router;
