08652ed35f2c57be817a4a59d14cfa337e6c4c0d
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-01 13:15:25 +0300

    Filtering search according to pet_name

--- PurrfectPals-FE/src/Component/Search.js
+++ PurrfectPals-FE/src/Component/Search.js
@@ -1,27 +1,40 @@
-import * as React from 'react';
-import { Searchbar } from 'react-native-paper';
-import axios from 'axios';
+import { useState } from "react";
+import { Searchbar } from "react-native-paper";
+import axios from "axios";
 
+const SearchComponent = ({ posts, setPosts }) => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-const MyComponent = () => {
-  const [searchQuery, setSearchQuery] = React.useState('');
-
-  const onChangeSearch = query => setSearchQuery(query);
-
+  const onChangeSearch = (query) => {
+    // console.warn(posts);
+    let filtered = posts?.filter((x) => x.pet_name?.includes(query));
+    setSearchQuery(filtered);
+    setPosts(filtered);
+    if (query === "") {
+      axios({
+        method: "GET",
+        url: `http://192.168.1.4:5000/user/getPosts`,
+      }).then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+      });
+    }
+  };
 
   return (
     <Searchbar
       placeholder="Search"
       onChangeText={onChangeSearch}
       value={searchQuery}
-      style={{marginTop:20,
-         marginBottom:20,
-         borderRadius:30,
-         elevation:2,
-         backgroundColor:'white',
-        }}
+      style={{
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 30,
+        elevation: 2,
+        backgroundColor: "white",
+      }}
     />
   );
 };
 
-export default MyComponent;
+export default SearchComponent;
