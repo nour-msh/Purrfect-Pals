e3db927ad99ec0c6445f33f743e4025a0d5a6244
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 22:29:28 +0300

    Handling a new task added

--- PurrfectPals-FE/src/Component/TaskInput.js
+++ PurrfectPals-FE/src/Component/TaskInput.js
@@ -1,15 +1,21 @@
 import React, {useState} from 'react';
-import { KeyboardAvoidingView, View, TextInput, TouchableOpacity, } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, View, TextInput, TouchableOpacity, } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons'; 
 
 export default TaskInputField = (props) => {
+    const [task, setTask] = useState();
 
+    const handleAddTask = (value) => {
+        props.addTask(value);
+        setTask(null);
+    }
 
     return (
         <KeyboardAvoidingView 
-        style={styles.container}>
-        <TextInput style={styles.inputField} placeholder={'Write a task'} placeholderTextColor={'#fff'}/>
-        <TouchableOpacity>
+        style={styles.container}
+      >
+        <TextInput style={styles.inputField} value={task} onChangeText={text => setTask(text)} placeholder={'Write a task'} placeholderTextColor={'#fff'}/>
+        <TouchableOpacity onPress={() => handleAddTask(task)}>
           <View style={styles.button}>
               <MaterialIcons name="keyboard-arrow-up" size={24} color="black" />
           </View>
