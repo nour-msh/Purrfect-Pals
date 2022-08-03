5d2d873acaff7b561ae0bec58898b285e2503e36
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 20:57:09 +0300

    Changing navigation pathway

--- PurrfectPals-FE/src/Component/Vet.js
+++ PurrfectPals-FE/src/Component/Vet.js
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet, Image } from "react-native";
 
-function Vet({ nav, name }) {
+function Vet({ nav, vetData }) {
+  console.log(vetData)
   return (
     <View style={styles.container}>
       <View style={styles.upperContainer}></View>
@@ -12,57 +13,62 @@ function Vet({ nav, name }) {
       </View>
 
       <View style={styles.bottomContainer}>
-        <Text style={styles.vetName} onPress={() => nav.navigate("VetAppointment")}>
-          Dr. {name}
+        <Text
+          style={styles.vetName}
+          onPress={() =>
+            nav.navigate("VetPortfolio", { dataFromVet:  vetData })
+          }
+        >
+          Dr. {vetData.full_name}
         </Text>
       </View>
     </View>
   );
 }
 
-const styles=StyleSheet.create({
-  container:{
-    marginTop:50,
-    display:'flex',
-    justifyContent:'center',
-    marginRight:20
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    display: "flex",
+    justifyContent: "center",
+    marginRight: 20,
   },
-  upperContainer:{
-    width:162,
-    height:103,
-    backgroundColor:'#FF914A',
-    borderTopRightRadius:20,
-    borderTopLeftRadius:20,
+  upperContainer: {
+    width: 162,
+    height: 103,
+    backgroundColor: "#FF914A",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
     opacity: 0.7,
-    position:'relative'
+    position: "relative",
   },
-  imageContainer:{
-    height:240,
-    width:162,
-    backgroundColor:'none',
-    position:'absolute',
+  imageContainer: {
+    height: 240,
+    width: 162,
+    backgroundColor: "none",
+    position: "absolute",
   },
-  Doctor1:{
-    height:153,
-    width:162,
+  Doctor1: {
+    height: 153,
+    width: 162,
   },
-  bottomContainer:{
-    width:162,
-    height:50,
-    backgroundColor:'white',
-    borderBottomRightRadius:20,
-    borderBottomLeftRadius:20,
-    borderWidth:1,
-    borderBottomColor:'#e5e5e5',
-    borderLeftColor:'#e5e5e5',
-    borderRightColor:'#e5e5e5',
-    borderTopColor:'white'
+  bottomContainer: {
+    width: 162,
+    height: 50,
+    backgroundColor: "white",
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderWidth: 1,
+    borderBottomColor: "#e5e5e5",
+    borderLeftColor: "#e5e5e5",
+    borderRightColor: "#e5e5e5",
+    borderTopColor: "white",
   },
-  vetName:{
-    textAlign:'center',
-    fontSize:15,
-    padding:10,
-    fontWeight:'200'
-  }
-})
+  vetName: {
+    textAlign: "center",
+    fontSize: 15,
+    padding: 10,
+    fontWeight: "200",
+  },
+});
 export default Vet;
