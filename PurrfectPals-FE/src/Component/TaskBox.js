c888903f9e7513f02d3f5bd2f71c2be0a501d906
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 22:14:20 +0300

    Adding task and index containers and delete icon

---
+++ PurrfectPals-FE/src/Component/TaskBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default TaskItem = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.indexContainer}>
+                <Text style={styles.index}>{props.index}</Text>
+            </View>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>{props.task}</Text>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={18} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+   
+});
