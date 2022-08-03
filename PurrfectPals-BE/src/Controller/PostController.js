2fa61450a67c96a59f7c1bb3b6a31843a8a36285
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 20:29:04 +0300

    Adding image to post controller

--- PurrfectPals-BE/src/Controller/PostController.js
+++ PurrfectPals-BE/src/Controller/PostController.js
@@ -1,32 +1,28 @@
-import { Post} from "../Model/Post.js";
+import { Post } from "../Model/Post.js";
 
-
-export async function addPost(req,res){
-    try{
-        const{
-            pet_name,
-            duration,
-            description,
-        }=req.body;
-        const post=new Post({
-            pet_name,
-            duration,
-            description,
-        })
-        await post.save();
-        res.status(201).send(post);
-    } catch(error){
-        res.status(400).json({message:error.message});
-    }
+export async function addPost(req, res) {
+  try {
+    const { pet_name, duration, description, image } = req.body;
+    const post = new Post({
+      pet_name,
+      image,
+      duration,
+      description,
+    });
+    await post.save();
+    res.status(201).send(post);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export async function getPosts(req,res){
-    try{
-        const results = await Post.find()
-        res.status(200).json(results)
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
+export async function getPosts(req, res) {
+  try {
+    const results = await Post.find();
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 }
