7b3f042ffc007a906e53b7281dee13afe94cbaa9
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 21:02:18 +0300

    Implementing addPortfolio function

--- PurrfectPals-BE/src/Controller/VetController.js
+++ PurrfectPals-BE/src/Controller/VetController.js
@@ -1,30 +1,32 @@
-import { Appointment } from '../Model/Appointment.js';
-import {User} from '../Model/User.js';
+import { Portfolio } from "../Model/Portfolio.js";
+import { User } from "../Model/User.js";
 
-export async function getVets(req,res){
-  try{
-    const results = await User.find({user_type:'1'})
-    res.status(200).json(results)
+export async function getVets(req, res) {
+  try {
+    const results = await User.find({ user_type: "1" });
+    res.status(200).json(results);
   } catch (error) {
     res.status(500).json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
 }
 
-export async function addAppointment(req,res){
-  try{
-      const{
-          date,
-          time,
-      }=req.body;
-      const appointment=new Appointment({
-          date,
-          time,
-      })
-      await appointment.save();
-      res.status(201).send(appointment);
-  } catch(error){
-      res.status(400).json({message:error.message});
+export async function addPortfolio(req, res) {
+  try {
+    const { vet_email, vet_number, vet_specialty, vet_story, vet_image, user_id } = req.body;
+    const portfolio = new Portfolio({
+      vet_email,
+      vet_number,
+      vet_specialty,
+      vet_story,
+      vet_image,
+      user_id,
+    });
+    await portfolio.save();
+    res.status(201).send(portfolio);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 }
+
