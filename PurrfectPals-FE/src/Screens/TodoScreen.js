c4e3d83510f6f49b5cd7a0a0952584966a577744
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-03 22:03:15 +0300

    Adding header to todo screen

---
+++ PurrfectPals-FE/src/Screens/TodoScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function Todo() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.heading}>TODO LIST</Text>
+    </View>
+  );
+}
