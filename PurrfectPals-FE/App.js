d1171b9beb0b959fa4ebec6f881fe324af2d1e4f
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-04 23:16:37 +0300

    Adding user's info to context

--- PurrfectPals-FE/App.js
+++ PurrfectPals-FE/App.js
@@ -3,9 +3,6 @@ import { createContext, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Stacks from "./src/Navigation/tabs";
 import React from "react";
-import DateBox from "./src/Component/DateBox";
-import { Time } from "react-native-gifted-chat";
-import TimeBox from "./src/Component/TimeBox";
 const ContextProvider = createContext();
 
 export const UserContext = createContext();
@@ -14,26 +11,31 @@ export default function App() {
   const [userId, setUserId] = useState("")
   const [userFullName, setUserFullName] = useState("")
   const [userPhoneNumber, setUserPhoneNumber] = useState("")
+  const [userImage, setUserImage] = useState("");
+  const [vetImage, setVetImage] = useState("");
+
   const [userToken, setUserToken] = useState("")
-  
+
   return (
     <>
-      <UserContext.Provider value={{ 
-                                    userId,
-                                    setUserId,
-                                    userToken, 
-                                    setUserToken,
-                                    userFullName, 
-                                    setUserFullName, 
-                                    userPhoneNumber, 
-                                    setUserPhoneNumber 
-                                    }}>
+      <UserContext.Provider value={{
+          userId,
+          setUserId,
+          userToken,
+          setUserToken,
+          userFullName,
+          setUserFullName,
+          userPhoneNumber,
+          setUserPhoneNumber,
+          userImage,
+          setUserImage,
+          vetImage,
+          setVetImage
+        }}>
         <NavigationContainer>
           <Stacks />
         </NavigationContainer>
-      </UserContext.Provider> 
-      {/* <TimeBox/> */}
-      {/* <DateBox/> */}
+      </UserContext.Provider>
     </>
   );
 }
