8fd7716a639ef0b9fcf50b56bd40b4827085b27a
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-04 19:49:03 +0300

    Removing other routes and keeping user routes only

--- PurrfectPals-BE/src/Routes/UserRoutes.js
+++ PurrfectPals-BE/src/Routes/UserRoutes.js
@@ -1,33 +1,26 @@
 import { Router } from "express";
-import { register, login } from "../Controller/AuthController.js";
-import { addPet, getPets } from "../Controller/PetController.js";
-import { addReview,getAllReviewsByPetId } from "../Controller/ReviewController.js";
-import { updateUserProfile } from "../Controller/UserController.js";
-import { getPosts,addPost } from "../Controller/PostController.js";
-import { getVets,addPortfolio,getPortfolios } from "../Controller/VetController.js";
-import { search } from "../Controller/SearchController.js";
-import {addTask} from "../Controller/TaskController.js";
-import {addMark} from "../Controller/LocationController.js";
-
 const router = Router();
 
+import { register, login } from "../Controller/AuthController.js";
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/addPet/:user_id", addPet);
-router.get("/getPets/:user_id", getPets);
-router.post("/addReview/:user_id/:pet_id", addReview);
-router.get("/getAllReviewsByPetId/:pet_id", getAllReviewsByPetId);
-router.get("/getVets", getVets);
+
+
+import { updateUserProfile } from "../Controller/UserController.js";
+import { getPosts,addPost } from "../Controller/PostController.js";
+import { search } from "../Controller/SearchController.js";
+import {addTask} from "../Controller/TaskController.js";
+import {addMark,getMark} from "../Controller/LocationController.js";
+
+
 router.put("/updateUserProfile/:id", updateUserProfile);
 router.post("/addPost", addPost);
 router.get("/getPosts", getPosts);
 router.get("/search/:key", search);
-router.post("/addPortfolio", addPortfolio);
 router.post("/addTask", addTask);
 router.post("/addMark", addMark);
-router.get("/getPortfolios/:id", getPortfolios);
-
+router.post("/getMark", getMark);
 
 
 export default router;
