932ef90d289dbe80010dbb038cd792cdf7df1b67
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-07 22:44:24 +0300

    Adding info as props

--- purrfectpals-frontend/src/Component/DateBox.js
+++ purrfectpals-frontend/src/Component/DateBox.js
@@ -1,37 +1,29 @@
-import { StyleSheet,Text,View} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
-function DateBox(){
-    return(
-        <View style={styles.container}>
-            {/* <TextInput style={styles.dayNum}>4</TextInput> */}
-            <TextInput style={styles.day}>Sat</TextInput>
-        </View>
-    )}
+function DateBox({info}) {
+  console.warn(info);
+  return (
+    <View style={styles.containerDate}>
+      <Text style={styles.day}>{info}</Text>
+    </View>
+  );
+}
 
-const styles=StyleSheet.create({
-    container:{
-        width:"90%",
-        height:40,
-        borderRadius:15,
-        backgroundColor:'white',
-        marginTop:10,
-        marginLeft:20,
-        elevation:5
-    },
-    dayNum:{
-        textAlign:'center',
-        marginTop:10,
-        fontWeight:'bold',
-        fontSize:20
-    },
-    day:{
-        textAlign:'center',
-        marginTop:10,
-        fontWeight:'100'
-
-    }
-
-
-})
-export default DateBox
+const styles = StyleSheet.create({
+  containerDate: {
+    width: "90%",
+    height: 40,
+    borderRadius: 15,
+    backgroundColor: "white",
+    marginTop: 10,
+    marginLeft: 20,
+    elevation: 5,
+  },
+  day: {
+    textAlign: "center",
+    marginTop: 10,
+    fontWeight: "100",
+  },
+});
+export default DateBox;
