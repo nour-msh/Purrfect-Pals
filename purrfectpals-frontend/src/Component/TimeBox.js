c945bd4d47d05f15177c404f9d3a71c9b1947936
Nour Mshawrab <mshawrabnour@gmail.com>
2022-08-07 22:43:49 +0300

    Adding props

--- purrfectpals-frontend/src/Component/TimeBox.js
+++ purrfectpals-frontend/src/Component/TimeBox.js
@@ -1,31 +1,28 @@
-import { StyleSheet,View,TextInput} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-
-function TimeBox(){
-return(
-        <View style={styles.container}>
-            <TextInput style={styles.time}>9:00</TextInput>
-        </View>
-    )
+function TimeBox(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.time}>{props.info}</Text>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-    container:{
-        width:79,
-        height:33,
-        borderRadius:13,
-        backgroundColor:'white',
-        marginLeft:20,
-        elevation:5,
-        marginBottom:10
-
-    },
-    time:{
-        textAlign:'center',
-        padding:8,
-        fontWeight:'100'
-    }
-
-
-})
+const styles = StyleSheet.create({
+  container: {
+    width: 79,
+    height: 33,
+    borderRadius: 13,
+    backgroundColor: "white",
+    marginLeft: 20,
+    elevation: 5,
+    marginBottom: 10,
+  },
+  time: {
+    textAlign: "center",
+    padding: 8,
+    fontWeight: "100",
+  },
+});
 export default TimeBox;
+
